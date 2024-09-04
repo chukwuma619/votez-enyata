6ccc3d94e58e91ef33c7237035fad3b33b031217
Chukwuma Ebube <chukwuma619@gmail.com>
2024-09-04 23:05:36 +0100

    feat: add theme provider and made changes to layout

---
+++ src/components/theme-context.tsx
@@ -0,0 +1,46 @@
+'use client';
+import {
+  createContext,
+  useState,
+  useEffect,
+  SetStateAction,
+  Dispatch,
+} from 'react';
+export const ThemeContext = createContext<{
+  theme: 'light' | 'system' | 'dark';
+  setTheme: Dispatch<SetStateAction<'light' | 'system' | 'dark'>>;
+}>({ theme: 'system', setTheme: () => {} });
+
+export default function ThemeProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [theme, setTheme] = useState<'light' | 'system' | 'dark'>('system');
+
+  useEffect(() => {
+    if (localStorage.theme === 'dark') {
+      document.documentElement.classList.add('dark');
+      setTheme('dark');
+    } else if (localStorage.theme === 'light') {
+      document.documentElement.classList.remove('dark');
+      setTheme('light');
+    } else if (
+      !('theme' in localStorage) &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches
+    ) {
+      document.documentElement.classList.add('dark');
+    } else if (
+      !('theme' in localStorage) &&
+      !window.matchMedia('(prefers-color-scheme: dark)').matches
+    ) {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
